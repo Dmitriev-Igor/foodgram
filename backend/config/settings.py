e65d23244c8a6e5ce44b6c89f0b3d76e2cd6e3a7
Dmitiriev-Igor <dmitriev.lacosta87@yadnex.ru>
2025-04-24 17:30:57 +0300

    Что-то ещё поправил

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,10 +111,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/backend_static/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

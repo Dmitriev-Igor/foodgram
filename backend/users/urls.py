47661aa37830d11442dbf243a43e4da41916134d
Dmitiriev-Igor <dmitriev.lacosta87@yadnex.ru>
2025-04-29 23:02:33 +0300

    Что с импортами?

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
 router = DefaultRouter()
-# Будет доступно по /api/users/
+
 router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),  # Подключение UserViewSet
+    path('', include(router.urls)),
 ]

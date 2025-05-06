be2c20e80cea2a11310252eec369329c0de8354c
Dmitiriev-Igor <dmitriev.lacosta87@yadnex.ru>
2025-05-06 14:25:01 +0300

    Вроде всё получилось!

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,14 @@
-"""Настройки URL для проекта Foodgram."""
+# foodgram/urls.py
 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('tags', TagViewSet, basename='tags')
-router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('recipes.urls')),
     path('api/', include('users.urls')),
-    path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
 
